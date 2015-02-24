f2f48c67ce7af1413e68af970c8c01e3031328b1
Incognito <OnlineExpert001@gmail.com>
2015-02-24 14:16:38 +0200

    Split Gem is compatible with Rails API Gem

--- lib/split/engine.rb
+++ lib/split/engine.rb
@@ -2,8 +2,8 @@ module Split
   class Engine < ::Rails::Engine
     initializer "split" do |app|
       if Split.configuration.include_rails_helper
-        ActionController::Base.send :include, Split::Helper
-        ActionController::Base.helper Split::Helper
+        ActionController::API.send :include, Split::Helper
+        ActionController::API.helper Split::Helper
       end
     end
   end
